d2aad5cbf6262312feb61c19121ef2a5c04b687b
Derek Reeve <derek@devbootcamp.com>
2014-10-28 13:45:43 -0700

    First failing test!

--- spec/index_controller_spec.rb
+++ spec/index_controller_spec.rb
@@ -1,1 +1,11 @@
 require_relative 'spec_helper'
+
+describe "index controller" do
+  it "responds with a successful status" do
+    # Given
+    # When
+    get '/articles'
+    # Then
+    expect(last_response.status).to eq(200)
+  end
+end
