9cf28e85d112e53f518be021cb94cd1c6c8efecc
Derek Reeve <derek@devbootcamp.com>
2014-10-29 10:27:50 -0700

    Updates from lecture!

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,9 +1,13 @@
 get '/articles' do
-  
   @articles = Article.all
-  erb :index
+  erb :articles
+end
+
+post '/articles' do
+  Article.create(params)
 end
 
 get '/articles/:id' do
-  
+  @article = Article.find(params[:id])
+  erb :article 
 end
--- spec/index_controller_spec.rb
+++ spec/index_controller_spec.rb
@@ -1,33 +1,67 @@
 require_relative 'spec_helper'
 
-describe "index controller" do
+describe "index_controller" do
+  before do
+    Article.delete_all
+  end
+
   describe "get /articles" do
     it "responds with a successful status" do
       # Given
+      # setup the world that has to exist for the test to even do something
       # When
       get '/articles'
       # Then
       expect(last_response.status).to eq(200)
     end
 
-    it "responds with articles that are in the database" do
+    it "responds with at least an article" do
       # Given
-      article = Article.create(title: "An Article Title")
+      Article.create(title: "An article title")
       # When
       get '/articles'
       # Then
-      expect(last_response.body).to include("An Article Title")
+      expect(last_response.body).to include("An article title")
     end
   end
 
   describe "get /articles/:id" do
-    it "responds with a successful status" do
+    
+    it "responds with success" do
       # Given
-      article = Article.create(title: "An Article Title")
+      article = Article.create(title: "Another Article title")
       # When
       get "/articles/#{article.id}"
       # Then
       expect(last_response.status).to eq(200)
+    end
+
+    it "responds with Article data" do
+      # Given
+      article = Article.create(title: "Another Article title")
+      # When
+      get "/articles/#{article.id}"
+      # Then
+      expect(last_response.body).to include("Another Article title")
+    end
+  end
+
+  describe "post /articles" do
+    it "responds with a 200" do
+      # Given
+      # When
+      post '/articles'
+      # Then
+      expect(last_response.status).to eq(200)
+    end
+
+    it "creates a new Article when a new article is submitted" do
+      # Given
+      article_length_before = Article.count
+      # When
+      expect {
+        post '/articles', {title: "An article title"}
+      }.to change { Article.count }.by(1)
     end
   end
 end
