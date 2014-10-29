a5288f16042a30bf98fa45b5e686b9ec7c94c279
Derek Reeve <derek@devbootcamp.com>
2014-10-28 17:28:58 -0700

    Minor refactor to controller spec.

--- spec/index_controller_spec.rb
+++ spec/index_controller_spec.rb
@@ -1,12 +1,23 @@
 require_relative 'spec_helper'
 
 describe "index controller" do
-  it "responds with a successful status" do
-    # Given
-    # When
-    get '/articles'
-    # Then
-    expect(last_response.status).to eq(200)
+  describe "get /articles" do
+    it "responds with a successful status" do
+      # Given
+      # When
+      get '/articles'
+      # Then
+      expect(last_response.status).to eq(200)
+    end
+
+    it "responds with articles that are in the database" do
+      # Given
+      article = Article.create(title: "An Article Title")
+      # When
+      get '/articles'
+      # Then
+      expect(last_response.body).to include("An Article Title")
+    end
   end
 
   it "responds with articles that are in the database" do
